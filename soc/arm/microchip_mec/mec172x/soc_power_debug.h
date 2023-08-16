e50e6e75445276738715dc68d15f18340849590b
Jeff Daly <jeffd@silicom-usa.com>
2023-08-16 08:57:56 -0400

    Microchip MEC172X: Make PM debug helper board specific
    PM debug for MEC172X was initially implemented as SoC-specific code,
    with the changes to the GPIO control this commit makes it possible to
    make debug support code board-specific.
    
    Signed-off-by: Jeff Daly <jeffd@silicom-usa.com>

--- soc/arm/microchip_mec/mec172x/soc_power_debug.h
+++ soc/arm/microchip_mec/mec172x/soc_power_debug.h
@@ -7,34 +7,13 @@
 #ifndef __SOC_POWER_DEBUG_H__
 #define __SOC_POWER_DEBUG_H__
 
-/* #define SOC_SLEEP_STATE_GPIO_MARKER_DEBUG */
+#include <zephyr/toolchain.h>
+__weak void pm_debug_function(uint32_t debug);
 
-#ifdef SOC_SLEEP_STATE_GPIO_MARKER_DEBUG
+#define PM_DEBUG_ENTER	1
+#define PM_DEBUG_EXIT	0
 
-/* Select a gpio not used. LED4 on EVB. High = ON */
-#define DP_GPIO_ID		MCHP_GPIO_0241_ID
-
-/* output drive high */
-#define PM_DP_ENTER_GPIO_VAL	0x10240U
-/* output drive low */
-#define PM_DP_EXIT_GPIO_VAL	0x0240U
-
-static inline void pm_dp_gpio(uint32_t gpio_ctrl_val)
-{
-	struct gpio_regs * const regs =
-		(struct gpio_regs * const)(DT_REG_ADDR(DT_NODELABEL(gpio_000_036)));
-
-	regs->CTRL[DP_GPIO_ID] = gpio_ctrl_val;
-}
-
-#endif
-
-#ifdef DP_GPIO_ID
-#define PM_DP_ENTER()	pm_dp_gpio(PM_DP_ENTER_GPIO_VAL)
-#define PM_DP_EXIT()	pm_dp_gpio(PM_DP_EXIT_GPIO_VAL)
-#else
-#define PM_DP_ENTER()
-#define PM_DP_EXIT()
-#endif
+#define PM_DP_ENTER()	pm_debug_function(PM_DEBUG_ENTER)
+#define PM_DP_EXIT()	pm_debug_function(PM_DEBUG_EXIT)
 
 #endif /* __SOC_POWER_DEBUG_H__ */
